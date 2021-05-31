f69f4af3b342b76c039c335862ff6120b79128b4
SGierszewski <stefaniegierszewski@hotmail.com>
2021-05-31 20:27:55 +0200

    ✨ Add mor router paths

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Register from './Page/Register';
+import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
 import MovieDetails from './pages/MovieDetails/MovieDetails';
+import Homescreen from './pages/Homescreen/Homescreen';
+import TVShowDetails from './pages/TVShowDetails/TVShowDetails';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <p>Home</p>
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/forgotpassword">
+          <ForgotPassword />
+        </Route>
+        <Route path="/homescreen">
+          <Homescreen />
         </Route>
         <Route path="/movie/:name">
           <MovieDetails />
+        </Route>
+        <Route path="/show/:name">
+          <TVShowDetails />
         </Route>
       </Switch>
     </BrowserRouter>
